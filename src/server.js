4bccbc7ec0193f1d69dc722b0d4b00a3fc11d0fd
Junaid Mansur Ifti <bsse1027@iit.du.ac.bd>
2021-04-07 13:51:43 +0600

    3rd

--- src/server.js
+++ src/server.js
@@ -6,3 +6,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 const server=http.createServer(app);
 const io=socketIO(server);
+
+io.on('connection',(socket)=>{
+
+    console.log("Client Connection Established");
+    socket.emit('message','Hello');
+
+});
+
+
+
+server.listen(port,()=>{
+
+    console.log("Server started succesfully.");
+});
