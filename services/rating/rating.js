2517979c4ce6a2162be977feabadbaf393932154
Junaid Mansur Ifti <28111997>
2021-04-20 22:15:44 +0600

    Docker files have been created. Only DB is not fixed

--- services/rating/rating.js
+++ services/rating/rating.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const connectionURL = 'mongodb://127.0.0.1:27017';
+const connectionURL = 'mongodb://192.168.0.107:27017';
 const databaseName = 'ride-sharing';
 
 const app=express();
--- services/rideSharing/rs.js
+++ services/rideSharing/rs.js
@@ -71,7 +71,7 @@ function pairMatch()
                 "fair":cost
             };
 
-            axios.post('http://localhost:3002/mp',
+            axios.post('http://192.168.0.107:3002/mp',
             
                 jsonMatch
                 
